3f72274289b2b420cb2d57d6125b6c41f0d440b3
muramarch <58620514+muramarch@users.noreply.github.com>
2023-06-08 18:28:58 +0600

    Update openai_integration.py
    changed the value of the max token

--- openai_integration.py
+++ openai_integration.py
@@ -7,7 +7,7 @@ def generate_response(message):
     response = openai.Completion.create(
         engine='text-davinci-003',
         prompt=message,
-        max_tokens=50,
+        max_tokens=100,
         temperature=0.7,
         n=1,
         stop=None
